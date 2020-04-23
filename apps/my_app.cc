f3c3cd7086d4fa3e75b43d7ebd4401d1972fd6bd
AllanHuang853 <shengqi5@illinois.edu>
2020-04-22 22:15:12 -0700

    Made simple UI, loaded the music

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -5,24 +5,182 @@
 #include <CinderImGui.h>
 #include <cinder/app/App.h>
 #include <cinder/gl/wrapper.h>
+#include <iostream>
+
+#include <cinder/Font.h>
+#include <cinder/Text.h>
+#include <cinder/Vector.h>
+#include <cinder/gl/draw.h>
+#include <cinder/gl/gl.h>
 
 namespace myapp {
 
 using cinder::app::KeyEvent;
+using cinder::Color;
+using cinder::ColorA;
+using cinder::Rectf;
+using cinder::TextBox;
+using std::string;
 
-MyApp::MyApp() { }
+MyApp::MyApp() :
+  state_{PageState::kFirstPage} {}
+
 
 void MyApp::setup() {
-  ImGui::initialize();
+  
+  // Setting up the songs
+  cinder::audio::SourceFileRef songOneFile =
+      cinder::audio::load(cinder::app::loadAsset("star.mp3"));
+  star_ = cinder::audio::Voice::create(songOneFile);
+
+  cinder::audio::SourceFileRef songTwoFile =
+      cinder::audio::load(cinder::app::loadAsset("birthday.mp3"));
+  birthday_ = cinder::audio::Voice::create(songTwoFile);
+
+  cinder::audio::SourceFileRef songThreeFile =
+      cinder::audio::load(cinder::app::loadAsset("tetris.mp3"));
+  tetris_ = cinder::audio::Voice::create(songThreeFile);
 }
 
-void MyApp::update() { }
+template <typename C>
+void PrintText(const string& text, const C& color, const cinder::ivec2& size,
+               const cinder::vec2& loc) {
+  cinder::gl::color(color);
+
+  auto box = TextBox()
+      .alignment(TextBox::CENTER)
+      .font(cinder::Font(kNormalFont, 40))
+      .size(size)
+      .color(color)
+      .backgroundColor(ColorA(1, 1, 0, 0))
+      .text(text);
+
+  const auto box_size = box.getSize();
+  const cinder::vec2 locp = {loc.x - box_size.x / 2, loc.y - box_size.y / 2};
+  const auto surface = box.render();
+  const auto texture = cinder::gl::Texture::create(surface);
+  cinder::gl::draw(texture, locp);
+}
+
+
+void MyApp::update() {
+  
+  if (state_ == PageState::playEasy) {
+    star_->start();
+  } 
+  if (state_ == PageState::playMed) {
+    birthday_->start();
+  } 
+  if (state_ == PageState::playHard) {
+    tetris_->start();
+  } 
+  if (state_ == PageState::goBack) {
+    star_->stop();
+    birthday_->stop();
+    tetris_->stop();
+  }
+}
+
 
 void MyApp::draw() {
-  cinder::gl::clear(cinder::Color(0,0,0));
-  ImGui::Text("Hello World");
+  draw_main();
+  
+  if (state_ == PageState::nextPage) {
+    if (state_ == PageState::goBack) {
+      draw_main();
+    }
+    draw_select();
+  }
+
+  if (state_ == PageState::playEasy) {
+    draw_sheets();
+    if (state_ == PageState::goBack && state_ != PageState::nextPage) {
+      draw_select();
+    }
+  } 
+  
+  if (state_ == PageState::playMed) {
+    draw_sheets();
+  } 
+  
+  if (state_ == PageState::playHard) {
+    draw_sheets();
+  }
 }
 
-void MyApp::keyDown(KeyEvent event) { }
+void MyApp::draw_main() {
+  cinder::gl::clear(cinder::Color(1,1,1));
+
+  const cinder::vec2 center = getWindowCenter();
+  const cinder::ivec2 size = {1300, 400};
+  const Color color = {1, 0, 0};
+  //size_t row = 0;
+  PrintText("Rhythm Master", color, size, center);
+
+  const cinder::vec2 center2 = getWindowCenter();
+  const cinder::ivec2 size2 = {1300, 220};
+  const Color color2 = Color::black();
+  PrintText("Press s to select", color2, size2, center2);
+
+  const cinder::vec2 center3 = getWindowCenter();
+  const cinder::ivec2 size3 = {1300, 50};
+  const Color color3 = Color::black();
+  PrintText("Press esc to quit", color3, size3, center3);
+}
+
+void MyApp::draw_select() {
+  cinder::gl::clear(cinder::Color(1,1,1));
+  const cinder::vec2 center = getWindowCenter();
+  const cinder::ivec2 size = {1300, 400};
+  const Color color = Color::black();
+  //size_t row = 0;
+  PrintText("Easy (press 1)", color, size, center);
+
+  const cinder::vec2 center2 = getWindowCenter();
+  const cinder::ivec2 size2 = {1300, 220};
+  const Color color2 = Color::black();
+  PrintText("Medium (press 2)", color2, size2, center2);
+
+  const cinder::vec2 center3 = getWindowCenter();
+  const cinder::ivec2 size3 = {1300, 50};
+  const Color color3 = Color::black();
+  PrintText("Hard (press 3)", color3, size3, center3);
+}
+
+void MyApp::draw_sheets() {
+  cinder::gl::clear(cinder::Color(1,1,1));
+  const cinder::vec2 center = getWindowCenter();
+  const cinder::ivec2 size = {1300, 400};
+  const Color color = Color::black();
+  //size_t row = 0;
+  PrintText("Music", color, size, center);
+}
+
+
+void MyApp::keyDown(KeyEvent event) {
+  if (event.getCode() == KeyEvent::KEY_ESCAPE) {
+    exit(0);
+  }
+  
+  if (event.getChar() == 's') {
+    state_ = PageState::nextPage;
+  }
+  
+  if (event.getCode() == KeyEvent::KEY_LEFT) {
+    state_ = PageState::goBack;
+  }
+  
+  if (event.getCode() == KeyEvent::KEY_1) {
+    state_ = PageState::playEasy;
+  }
+
+  if (event.getCode() == KeyEvent::KEY_2) {
+    state_ = PageState::playMed;
+  }
+
+  if (event.getCode() == KeyEvent::KEY_3) {
+    state_ = PageState::playHard;
+  }
+}
 
 }  // namespace myapp
--- apps/my_app.h
+++ apps/my_app.h
@@ -4,9 +4,22 @@
 #define FINALPROJECT_APPS_MYAPP_H_
 
 #include <cinder/app/App.h>
+#include <cinder/app/App.h>
+#include <cinder/audio/audio.h>
+#include <cinder/gl/gl.h>
 
+#include <random>
+#include <string>
+#include <vector>
 
 namespace myapp {
+
+enum class PageState {
+  kFirstPage, nextPage,
+  goBack, playEasy, playMed, playHard
+};
+
+const char kNormalFont[] = "Arial";
 
 class MyApp : public cinder::app::App {
  public:
@@ -14,7 +27,17 @@ class MyApp : public cinder::app::App {
   void setup() override;
   void update() override;
   void draw() override;
+  void draw_select();
+  void draw_main();
+  void draw_sheets();
   void keyDown(cinder::app::KeyEvent) override;
+
+ private: 
+  PageState state_;
+  cinder::audio::VoiceRef star_;
+  cinder::audio::VoiceRef birthday_;
+  cinder::audio::VoiceRef tetris_;
+  
 };
 
 }  // namespace myapp
